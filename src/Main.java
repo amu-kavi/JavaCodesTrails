007b99ec364256ccde093da2968820653a2455ed
Kavitha Murugan <mkmkkavitha@gmail.com>
2020-11-12 01:37:40 +0530

    My first commit of Java Files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello World! from the intelliJ! My first code on  intelliJ IDE");
+        String Name ="";
+        System.out.println("Whats your Name ???");
+        Scanner input = new Scanner(System.in);
+        Name =input.nextLine();
+        System.out.println("Name : "+Name);
     }
 }
