b0ab94aeb861a4ae6718063200e37c0b8569fcbd
Kavitha Murugan <mkmkkavitha@gmail.com>
2020-11-12 01:51:14 +0530

    My first commit of Java Files

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,16 @@
+public class PrimitiveDataTypes{
+	
+	public static void main(String[] args){
+		int studentAge = 15;
+		double studentGPA = 7.7;
+		char StudentFirstInitial = 'K';
+		char StudentLastInitial = 'M';
+		boolean isFirstClassPass = true;
+	  System.out.println("Hello World! Its me Kavitha Murugan!");
+	  System.out.println("Student's Age : " + studentAge );
+	  System.out.println("Student's GPA : " + studentGPA);
+	  System.out.println("Student's First Initial : " + StudentFirstInitial );
+	  System.out.println("Student's Last Initial : " + StudentLastInitial );
+	  System.out.println("Student's Has Passed in first class : " + isFirstClassPass );
+	}
+}
