faba58b15062b0b377b2e70374895c3b61672634
Kavitha Murugan <mkmkkavitha@gmail.com>
2020-11-12 00:54:25 +0530

    My first commit of Java Files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World! from the intelliJ! My first from intelliJ IDE");
+        System.out.println("Hello World! from the intelliJ! My first code on  intelliJ IDE");
     }
 }
